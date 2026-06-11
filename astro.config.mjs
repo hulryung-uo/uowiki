20758559abf8b1add1aac076473b81efda84c063
Daekeun Kang <dkkang@huconn.com>
2026-06-11 13:21:52 +0900

    wiki(config): set site URL for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://uowiki.vercel.app',
 	integrations: [
 		starlight({
 			title: 'UO Wiki',
